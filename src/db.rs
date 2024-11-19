0f5f7c10c03bda17df2dd252ee336baf6b71f707
serabiasin <ahmadalfiazdzikri@gmail.com>
2024-11-19 16:23:09 +0700

    update mechanism db

--- src/db.rs
+++ src/db.rs
@@ -1,4 +1,15 @@
 use sqlx::{migrate::MigrateDatabase, sqlite::SqliteQueryResult, Sqlite, SqlitePool};
+use sqlx:: Row;
+
+// #[derive(Debug, FromRow)]
+
+// struct Books{
+//     id_book:i32,
+//     name_book:String,
+//     author_book:String,
+//     created_on:String,
+//     updated_on:String
+// }
 
 pub async fn create_schema(db_url: &str) -> Result<SqliteQueryResult, sqlx::Error> {
     let pool = SqlitePool::connect(&db_url).await?;
@@ -6,13 +17,15 @@ pub async fn create_schema(db_url: &str) -> Result<SqliteQueryResult, sqlx::Erro
     "
     CREATE TABLE IF NOT EXISTS books_db(
             id_book                   INTEGER PRIMARY KEY AUTOINCREMENT,
-            name_book                 TEXT,
+            name_book                 TEXT NOT NULL,
+            author_book               TEXT NOT NULL,
             created_on                DATETIME DEFAULT (datetime('now','localtime')),
-            updated_on                DATETIME DEFAULT (datetime('now','localtime')),
-            author_book               TEXT
+            updated_on                DATETIME DEFAULT (datetime('now','localtime'))
         );";
+    
     let result = sqlx::query(&qry).execute(&pool).await;
     pool.close().await;
+
     return result;
 }
 
@@ -25,10 +38,47 @@ pub async fn check_db(db_url:&str){
         }
     }
     let instances = SqlitePool::connect(&db_url).await.unwrap();
-    let qry = "INSERT INTO books_db (name_book) VALUES($1)";
-    let result = sqlx::query(&qry).bind("testing").execute(&instances).await;
+    let qry = "INSERT INTO books_db (name_book,author_book) VALUES($1,$2)";
+    let result = sqlx::query(&qry).bind("testing").bind("test 2").execute(&instances).await;
 
     instances.close().await;
 
     println!("{:?}", result);
+}
+
+
+
+pub async fn insert_db(db_url: &str,name_book: &str,author_book:&str){
+    let query="INSERT INTO books_db (name_book,author_book) VALUES($1,$2)";
+
+    let instances = SqlitePool::connect(&db_url).await.unwrap();
+
+    let execute_query=sqlx::query(&query).bind(name_book).bind(author_book).execute(&instances).await;
+
+
+    instances.close().await;
+    print!("{:?}",execute_query);
+}
+
+
+pub  async fn select_data_db(db_url:&str,id_books:&u32){
+
+    let query="SELECT * FROM books_db  WHERE id_book=$1";
+
+    let instances = SqlitePool::connect(&db_url).await.unwrap();
+
+    let buffer_exec=sqlx::query(&query).bind(id_books).fetch_all(&instances).await.unwrap();
+	let str_result = buffer_exec
+		.iter() //unpack the Vec<>
+		.map(|r| format!("{} - {}", r.get::<i32, _>("id_book"), r.get::<String, _>("name_book"))) //mapping the SqliteRow
+		.collect::<Vec<String>>()
+		.join(", ");
+
+	println!("\n== select tickets with Row:\n{}", str_result);
+
+
+    instances.close().await;
+
+
+
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 mod db;
-use crate::db::create_schema;
-use db::check_db;
+use db::{*};
 use sqlx::{migrate::MigrateDatabase, Sqlite, SqlitePool};
 
 #[async_std::main]
@@ -8,4 +7,7 @@ async fn main() {
     let db_url = String::from("sqlite://sqlite.db");
 
     check_db(&db_url).await; //menunggu proses selesai, karena async
+    insert_db(&db_url, "Mulyono adalah kita", "Joke 'O We").await;
+    let id:u32=2;
+    select_data_db(&db_url, &id).await;
 }
