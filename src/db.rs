abf86ffe67fa432b8c7b9777b895e10ac39bc648
serabiasin <ahmadalfiazdzikri@gmail.com>
2024-11-19 09:31:03 +0700

    begin creating db and testing query

--- src/db.rs
+++ src/db.rs
@@ -3,26 +3,13 @@ use sqlx::{migrate::MigrateDatabase, sqlite::SqliteQueryResult, Sqlite, SqlitePo
 pub async fn create_schema(db_url: &str) -> Result<SqliteQueryResult, sqlx::Error> {
     let pool = SqlitePool::connect(&db_url).await?;
     let qry =
-    "PRAGMA foreign_keys = ON ;
-    CREATE TABLE IF NOT EXISTS settings
-        (
-            settings_id             INTEGER PRIMARY KEY NOT NULL,
-            description             TEXT                NOT NULL,
-            created_on              DATETIME DEFAULT (datetime('now','localtime')),
-            updated_on              DATETIME DEFAULT (datetime('now','localtime')),
-            done                    BOOLEAN             NOT NULL DEFAULT 0
-        );
-    CREATE TABLE IF NOT EXISTS project
-        (
-            project_id                   INTEGER PRIMARY KEY AUTOINCREMENT,
-            product_name                 TEXT ,
-            created_on                   DATETIME DEFAULT (datetime('now','localtime')),
-            updated_on                   DATETIME DEFAULT (datetime('now','localtime')),
-            img_directory                TEXT NOT NULL,
-            out_directory                TEXT NOT NULL,
-            status                       TEXT NOT NULL,
-            settings_id                  INTEGER  NOT NULL DEFAULT 1,
-            FOREIGN KEY (settings_id)    REFERENCES settings (settings_id) ON UPDATE SET NULL ON DELETE SET NULL
+    "
+    CREATE TABLE IF NOT EXISTS books_db(
+            id_book                   INTEGER PRIMARY KEY AUTOINCREMENT,
+            name_book                 TEXT,
+            created_on                DATETIME DEFAULT (datetime('now','localtime')),
+            updated_on                DATETIME DEFAULT (datetime('now','localtime')),
+            author_book               TEXT
         );";
     let result = sqlx::query(&qry).execute(&pool).await;
     pool.close().await;
@@ -38,7 +25,7 @@ pub async fn check_db(db_url:&str){
         }
     }
     let instances = SqlitePool::connect(&db_url).await.unwrap();
-    let qry = "INSERT INTO settings (description) VALUES($1)";
+    let qry = "INSERT INTO books_db (name_book) VALUES($1)";
     let result = sqlx::query(&qry).bind("testing").execute(&instances).await;
 
     instances.close().await;
