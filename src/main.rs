2135cc4c620e8a74a74ea7fd15c9eadfe8c8212f
serabiasin <ahmadalfiazdzikri@gmail.com>
2024-11-19 09:17:28 +0700

    make it simple

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,11 @@
-mod utils;
-use crate::utils::create_schema;
-use sqlx::{migrate::MigrateDatabase, sqlite::SqliteQueryResult, Sqlite, SqlitePool};
-use std::result::Result;
+mod db;
+use crate::db::create_schema;
+use db::check_db;
+use sqlx::{migrate::MigrateDatabase, Sqlite, SqlitePool};
 
 #[async_std::main]
 async fn main() {
     let db_url = String::from("sqlite://sqlite.db");
-    if !Sqlite::database_exists(&db_url).await.unwrap_or(false) {
-        Sqlite::create_database(&db_url).await.unwrap();
-        match create_schema(&db_url).await {
-            Ok(_) => println!("Database created Sucessfully"),
-            Err(e) => panic!("{}", e),
-        }
-    }
-    let instances = SqlitePool::connect(&db_url).await.unwrap();
-    let qry = "INSERT INTO settings (description) VALUES($1)";
-    let result = sqlx::query(&qry).bind("testing").execute(&instances).await;
 
-    instances.close().await;
-
-    println!("{:?}", result);
+    check_db(&db_url).await; //menunggu proses selesai, karena async
 }
